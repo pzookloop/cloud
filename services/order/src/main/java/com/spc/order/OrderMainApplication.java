64d704724d2c6db703166d9cffbbf923440f9572
cocokalaco <pikakcoco@outlook.com>
2026-08-24 23:27:40 +0800

    nacos 编码方式监听配置变化

--- services/order/src/main/java/com/spc/order/OrderMainApplication.java
+++ services/order/src/main/java/com/spc/order/OrderMainApplication.java
@@ -1,8 +1,17 @@
 package com.spc.order;
 
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 
 @EnableDiscoveryClient // 开启服务发现
@@ -10,5 +19,28 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class OrderMainApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderMainApplication.class, args);
+    }
+
+    // 1.项目启动就监听配置文件的变化
+    // 2.拿到发生变化后的值
+    // 3.做相关的业务处理
+    @Bean
+    ApplicationRunner applicationRunner(NacosConfigManager manager) { // 项目一启动就启动runner, 给容器放一个bean
+        return args -> {
+            ConfigService configService = manager.getConfigService();
+            configService.addListener("service-order.properties", "DEFAULT_GROUP", new Listener() {
+                @Override
+                public Executor getExecutor() {
+                    return Executors.newFixedThreadPool(5);
+                }
+
+                @Override
+                public void receiveConfigInfo(String configInfo) {
+                    System.out.println("变化的信息: " + configInfo);
+                    System.out.println("邮件通知.....");
+                }
+            });
+            System.out.println("===============================");
+        };
     }
 }
