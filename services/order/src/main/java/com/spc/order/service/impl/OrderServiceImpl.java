90c05cd97a24c7269c12ae80cca813b51ebcba60
cocokalaco <pikakcoco@outlook.com>
2026-08-24 22:42:30 +0800

    手动取ip发远程调用(修改日志存储位置, 目前在C:\Users\user\logs\csp\)

--- services/order/src/main/java/com/spc/order/service/impl/OrderServiceImpl.java
+++ services/order/src/main/java/com/spc/order/service/impl/OrderServiceImpl.java
@@ -2,32 +2,54 @@ package com.spc.order.service.impl;
 
 import com.spc.order.service.OrderService;
 import com.spc.product.bean.Product;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import com.spc.order.bean.Order;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+
+@Slf4j
 @Service
 public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @Autowired
+    RestTemplate restTemplate;
+
     @Override
     public Order createOrder(Long productId, Long userId) {
         Order order = new Order();
         order.setId(1L);
         // Product product = getProductFromRemoteWithLoadBalancerAnnotation(productId);
 //        Product product = productFeignClient.getProductById(productId);
-//        BigDecimal totalAmount = product.getPrice().multiply(new BigDecimal(product.getNum()));
-//        order.setTotalAmount(totalAmount);
+        Product product = getProductFromRemote(productId);
+        BigDecimal totalAmount = product.getPrice().multiply(new BigDecimal(product.getNum()));
+        order.setTotalAmount(totalAmount);
         order.setTotalAmount(new BigDecimal(90));
         order.setUserId(userId);
         order.setNickname("abbc");
         order.setAddress("shanghai");
-//        order.setProductList(List.of(product));
-        order.setProductList(List.of());
+        order.setProductList(List.of(product));
         return order;
     }
 
     private Product getProductFromRemote(Long productId) {
-        return null;
+        // 1.获取所有商品服务所在机器的ip+port
+        List<ServiceInstance> instances = discoveryClient.getInstances("service-product");
+        ServiceInstance serviceInstance = instances.get(0);
+        String uri = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/product/" + productId;
+        // 发请求
+        log.info("远程请求: {}", uri);
+        Product product = restTemplate.getForObject(uri, Product.class);
+        log.info("远程请求成功: {}", product);
+        return product;
     }
 }
