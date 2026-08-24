b5ece5a40f952ca3c75988c5b6dad5663a45137a
cocokalaco <pikakcoco@outlook.com>
2026-08-24 23:15:13 +0800

    nacos 动态刷新配置

--- services/order/src/main/java/com/spc/order/controller/OrderController.java
+++ services/order/src/main/java/com/spc/order/controller/OrderController.java
@@ -4,19 +4,33 @@ package com.spc.order.controller;
 import com.spc.order.bean.Order;
 import com.spc.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@RefreshScope // 自动刷新来自nacos的配置
 @RestController
 public class OrderController {
 
     @Autowired
     private OrderService orderService;
 
+    @Value("${order.timeout}")
+    String orderTimeout;
+    @Value("${order.auto-confirm}")
+    String orderAutoConfirm;
+
     @GetMapping("/create")
     public Order createOrder(@RequestParam("userId") Long userId,
                              @RequestParam("productId") Long productId) {
         return orderService.createOrder(productId, userId);
+    }
+
+    @GetMapping("/config")
+    public String config() {
+        return "order.timeout" + orderTimeout + ", order.auto-confirm" + orderAutoConfirm;
     }
 }
