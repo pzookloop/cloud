f1157a1879df9215a27d477d7ef1a6c205da7b0a
cocokalaco <pikakcoco@outlook.com>
2026-08-24 21:27:06 +0800

    refactor

--- services/order/src/main/java/com/spc/product/OrderMainApplication.java
+++ services/order/src/main/java/com/spc/product/OrderMainApplication.java
@@ -1,8 +1,11 @@
-package com.spc.cloud;
+package com.spc.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@EnableDiscoveryClient // 开启服务发现
 @SpringBootApplication
 public class OrderMainApplication {
     public static void main(String[] args) {
