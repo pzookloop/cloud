04f58780f8b02fb6ff1b143b990a6ce21b34ae88
cocokalaco <pikakcoco@outlook.com>
2026-08-25 21:27:26 +0800

    sentinel 慢调用比例

--- services/product/src/main/java/com/spc/product/service/impl/ProductServiceImpl.java
+++ services/product/src/main/java/com/spc/product/service/impl/ProductServiceImpl.java
@@ -5,6 +5,7 @@ import com.spc.product.service.ProductService;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
 
 
 @Service
@@ -17,11 +18,11 @@ public class ProductServiceImpl implements ProductService {
         product.setPrice(new BigDecimal("99"));
         product.setProductName("apple-" + productId);
         product.setNum(2);
-//        try {
-//            TimeUnit.SECONDS.sleep(100);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException("超时");
-//        }
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            throw new RuntimeException("超时");
+        }
 
 //        int a = 1 / 0; 远程服务起不来, 超时, 异常都会走feign的兜底回调
         return product;
